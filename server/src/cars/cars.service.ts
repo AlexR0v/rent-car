74e1bf8c59c83f91d56ade23fde65716b5211716
AlexR0v <mail.ponomarev@yandex.ru>
2021-06-23 16:43:33 +0300

    feat: add server with data

---
+++ server/src/cars/cars.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Car } from './entities/car'
+import { Repository } from 'typeorm'
+import { NewCarInput } from './dto/car-input.dto'
+
+@Injectable()
+export class CarsService {
+  constructor(@InjectRepository(Car) private readonly carRepository: Repository<Car>) {
+  }
+
+  public async getAllCars(): Promise<Car[]> {
+    return await this.carRepository.find({}).catch((err) => {
+      throw new InternalServerErrorException()
+    })
+  }
+
+  public async addCar(newCarData: NewCarInput): Promise<Car> {
+    const newCar = this.carRepository.create(newCarData)
+    await this.carRepository.save(newCar).catch((err) => {
+      new InternalServerErrorException()
+    })
+
+    return newCar
+  }
+}
